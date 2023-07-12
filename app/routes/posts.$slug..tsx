e87b05fa3b8c2a457250fa85128dfbbffaf4204e
Joël Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2023-07-12 23:20:42 +0100

    add invariant for slug validation and marked for markdown rendering in html

--- app/routes/posts.$slug..tsx
+++ app/routes/posts.$slug..tsx
@@ -1,7 +1,29 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/post.server";
+import { marked } from "marked";
+import invariant from "tiny-invariant";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  const html = marked(post.markdown);
+  return json({ html, post });
+};
+
 export default function PostSlug() {
+  const { post } = useLoaderData<typeof loader>();
+
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">Some Post</h1>
+      <Link to="/posts" className="mb-4 text-xl text-blue-600 underline">
+        List of posts
+      </Link>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
     </main>
   );
 }
