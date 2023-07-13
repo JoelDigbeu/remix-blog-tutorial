0455ccba4c911fe579abe60f8e42a06194c89db7
Joël Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2023-07-13 11:02:51 +0100

    update post..tsx

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -5,6 +5,11 @@ import { getPost } from "~/models/post.server";
 import { marked } from "marked";
 import invariant from "tiny-invariant";
 
+type LoaderData = {
+  html: string;
+  title: string;
+};
+
 export const loader = async ({ params }: LoaderArgs) => {
   invariant(params.slug, `params.slug is required`);
 
@@ -12,18 +17,18 @@ export const loader = async ({ params }: LoaderArgs) => {
   invariant(post, `Post not found: ${params.slug}`);
 
   const html = marked(post.markdown);
-  return json({ html, post });
+  return json<LoaderData>({ html, title: post.title });
 };
 
 export default function PostSlug() {
-  const { post, html } = useLoaderData<typeof loader>();
+  const { title, html } = useLoaderData<typeof loader>();
 
   return (
     <main className="mx-auto max-w-4xl">
       <Link to="/posts" className="mb-4 text-xl text-blue-600 underline">
         List of posts
       </Link>
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
